484375dcf8708dfe2f4a84d79ffcfb3ab8247654
Jayavi11 <jayavinayakjayakumar@gmail.com>
2022-07-08 06:49:26 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentNameComponent } from './department-name/department-name.component';
+import { FacultyNameComponent } from './faculty-name/faculty-name.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentNameComponent,
+    FacultyNameComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
